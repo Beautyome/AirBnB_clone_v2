58303705aba2e679ade26255b529b1dd4ad196d6
Beautyome <beautyupeleomo@gmail.com>
2023-10-06 01:55:30 -0700

    airbnb

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from os import path
 
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['3.90.80.254', '54.174.136.222']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
