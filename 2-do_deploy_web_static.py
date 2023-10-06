ec284f6a3f76e8ff626db7b9f529badd66862ddd
Beautyome <beautyupeleomo@gmail.com>
2023-10-06 00:47:08 -0700

    airbnb

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,9 +5,7 @@ Deploy files to remote server using Fabric
 '''
 from fabric.api import env, put, run
 import os.path
-env.hosts = ['3.90.80.254', '54.174.136.222']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
+env.hosts = ['web1.beautyome.tech', 'web2.beautyome.tech']
 
 
 def do_deploy(archive_path):
